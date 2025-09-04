5e1a970d948ecd7cac5a45184057be6a609e9b0e
AESiR <cyborgkiller1008@gmail.com>
2025-09-04 18:18:59 +0530

    Add initial layout and page components for the studio environment, implementing a basic structure for rendering children and integrating the Next.js studio with Sanity configuration.

---
+++ app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
